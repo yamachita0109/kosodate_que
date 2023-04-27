17192ea2a78a1b4165bc6983ea34797f2234cf11
Yamashita Kazumasa <yamachita0109@gmail.com>
2023-04-28 00:02:23 +0900

    add device, mailcatcher.

--- Gemfile
+++ Gemfile
@@ -70,3 +70,6 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+# https://github.com/heartcombo/devise#starting-with-rails
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#top"
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions",
+    passwords: "users/passwords",
+    confirmations: "users/confirmations"
+  }
 end
