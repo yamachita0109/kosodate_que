646b5d5e497ba24f44e6bca92c9c8ce7e61dd483
Yamashita Kazumasa <yamachita0109@gmail.com>
2023-05-16 14:18:32 +0900

    meta tag.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,21 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def site_title(page_title = '')
+    base_title = 'パパママQA'
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
+  def site_description(page_description = '育児をしているパパママ必見の質問と回答のコミュニティサイトです。育児の悩みや疑問に答えたり、自分の育児の経験を共有したりできます。')
+    page_description
+  end
+
+  def site_keywords(page_keywords = '子育て,育児,質問,相談')
+    page_keywords
+  end
+
 end
