e30a0e8eb3d9a04f7770511fcd445a7e2614f873
Yamashita Kazumasa <yamachita0109@gmail.com>
2023-06-08 00:37:03 +0900

    質問編集

--- app/javascript/views/shared/Tags.vue
+++ app/javascript/views/shared/Tags.vue
@@ -4,6 +4,7 @@
       ref='tags'
       v-model="tags"
       @change="changeTags"
+      class="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
     >
   </p>
 </template>
