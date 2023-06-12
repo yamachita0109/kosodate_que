89acbc3014c9bd3e90b9fbc6e78e6edc75638060
Yamashita Kazumasa <yamachita0109@gmail.com>
2023-06-13 00:55:39 +0900

    modal.

--- app/javascript/views/question/edit/main.ts
+++ app/javascript/views/question/edit/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import Tags from '../../shared/Tags.vue'
+import BestAnswer from './BestAnswer.vue'
 
 function build() {
   document.addEventListener('DOMContentLoaded', () => {
     const input = document.querySelector('#question_tags')
     createApp(Tags, { inputForm: input }).mount('#vue-tags')
+
+    createApp(BestAnswer, { inputForm: input }).mount('#vue-best-answer')
+
   })
 }
 
