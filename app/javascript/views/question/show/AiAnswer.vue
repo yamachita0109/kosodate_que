58be7cdf58701a7898fb21b70bcfbe1ecf71da88
Yamashita Kazumasa <yamachita0109@gmail.com>
2023-06-14 22:00:10 +0900

    あいママ

--- app/javascript/views/question/show/AiAnswer.vue
+++ app/javascript/views/question/show/AiAnswer.vue
@@ -1,19 +1,75 @@
 <template>
-  <div class="p-5">
+  <div class="p-5 text-center">
     <button
       type="button"
       @click="clickAiAnswer()"
-      class="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-base"
-    >子育てAIに聞いてみる</button>
-    <p>
-      {{ answer }}
-    </p>
+      class="inline-flex text-white bg-rose-500 border-0 py-2 px-6 focus:outline-none hover:bg-rose-600 rounded text-base"
+    >教えて!<br/>あいママ</button>
+  </div>
+  <div
+    ref="modal"
+    tabindex="-1"
+    aria-hidden="true"
+    class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+  >
+    <div class="relative w-full max-w-2xl max-h-full">
+      <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+          <h3 class="text-xl font-semibold text-rose-600 dark:text-white">
+            教えて!あいママ
+          </h3>
+          <button
+            type="button"
+            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            @click="clickClose"
+          >
+            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            <span class="sr-only">Close modal</span>
+          </button>
+        </div>
+
+        <div
+          v-show="!answer"
+          class="p-6 space-y-6 text-center"
+      >
+          <p
+            class="font-medium text-base px-5 py-2.5 text-center mr-2 inline-flex items-center text-rose-600"
+          >
+            <svg aria-hidden="true" role="status" class="inline w-4 h-4 mr-3 text-rose-600 animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+            </svg>
+            考え中...
+          </p>
+        </div>
+
+        <div
+          v-show="answer"
+          class="p-6 space-y-6"
+        >
+          <p
+            v-html="answer.replace(/\n/g, '<br>')"
+            class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
+          ></p>
+        </div>
+
+        <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+          <button
+            type="button"
+            class="text-white bg-rose-500 border-0 py-2 px-6 focus:outline-none hover:bg-rose-600 rounded text-lg focus:ring-4 focus:outline-none focus:ring-rose-300"
+            @click="clickClose"
+          >閉じる</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import axios from 'axios'
+import { Modal } from 'flowbite'
+import type { ModalInterface } from 'flowbite'
 
 export default defineComponent({
   components: {
@@ -23,11 +79,20 @@ export default defineComponent({
       // question_id
       id: location.pathname.split('/').pop(),
       answer: '' as string,
+      modal: null as unknown as ModalInterface,
     }
+  },
+  mounted() {
+    const modal: HTMLElement = (this.$refs.modal as HTMLElement)
+    this.modal = new Modal(modal)
   },
   methods: {
     async clickAiAnswer() {
+      this.modal.show()
       this.answer = await this.getAnswer()
+    },
+    clickClose() {
+      this.modal.hide()
     },
     async getAnswer(): Promise<string> {
       const res = await axios.get('/api/ai/answer', { params: { question_id: this.id } })
