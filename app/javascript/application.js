98342567eb58df559d070b6fda1776e9bbe53a75
Yamashita Kazumasa <yamachita0109@gmail.com>
2023-06-21 23:06:23 +0900

    use importmap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,1 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-Turbo.session.drive = false
-import "controllers"
-
 import 'dist/main'
-import 'dist/question/show/main'
-import 'dist/question/edit/main'
-import 'dist/question/new/main'
-import 'dist/mypage/show/main'
-import 'dist/profile/show/main'
